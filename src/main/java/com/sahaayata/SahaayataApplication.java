86a964df97aa84829d3ea7d9575d7a25713449df
Darshit <balajidontulofficial@outlook.com>
2024-12-30 20:43:32 +0530

    second

---
+++ src/main/java/com/sahaayata/SahaayataApplication.java
@@ -0,0 +1,13 @@
+package com.sahaayata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahaayataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SahaayataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sahaayata/SahaayataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahaayata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahaayataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
